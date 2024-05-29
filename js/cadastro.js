431f58d4a1dd2879a55778390af0982a2b6d0d38
Niltinho77 <nilton.cn1992@aluno.ifsc.edu.br>
2024-05-29 12:47:12 -0300

    voltando a versao anterior do cadastro.js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,105 +1,45 @@
 import api from './api.js';
 
-let isAdmin = false;
+async function cadastrarProduto(event) {
+  event.preventDefault();
+  const codigo = document.getElementById('codigo').value.trim();
+  const nome = document.getElementById('nome').value.trim();
+  const imagem = document.getElementById('imagem').files[0];
 
-async function verificarAdmin() {
-  try {
-    const response = await api('/api/verifyToken', 'POST');
-    if (response.success) {
-      isAdmin = response.user.role === 'admin';
-    } else {
-      throw new Error('Token inválido');
-    }
-  } catch (error) {
-    console.error('Erro ao verificar token:', error);
-    localStorage.removeItem('token');
-    window.location.href = '/login.html';
-  }
-}
-
-// Função para remover um produto
-async function removerProduto(codigoProduto) {
-  if (!isAdmin) {
-    alert('Você não tem permissão para remover produtos.');
+  if (!codigo || !nome || !imagem) {
+    alert('Por favor, preencha todos os campos e selecione uma imagem.');
     return;
   }
+
+  const formData = new FormData();
+  formData.append('codigo', codigo);
+  formData.append('nome', nome);
+  formData.append('imagem', imagem);
+
   try {
-    const response = await api(`/api/produtos/${codigoProduto}`, 'DELETE');
-    if (response.success) {
-      alert(response.message || 'Produto removido com sucesso!');
-      atualizarTabelaEstoque(); // Atualiza a tabela após a remoção com sucesso
+    const response = await fetch('/api/produtos', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+      alert('Produto cadastrado com sucesso!');
+      window.close(); // Fechar a janela após o sucesso
     } else {
-      throw new Error(response.message || 'Erro ao remover produto.');
+      throw new Error(result.message || 'Erro ao cadastrar produto.');
     }
   } catch (error) {
-    console.error('Erro ao remover produto:', error);
-    alert(`Erro ao remover produto: ${error.message}`);
-  }
-}
-
-// Função para atualizar a tabela de estoque
-async function atualizarTabelaEstoque(filtro = '') {
-  try {
-    filtro = String(filtro);
-
-    const response = await api('/api/produtos', 'GET');
-    const produtos = response.produtos;
-    const tabelaEstoque = document.getElementById('tabelaEstoque');
-
-    const produtosFiltrados = produtos.filter(produto =>
-      produto.nome.toLowerCase().includes(filtro.toLowerCase()) ||
-      produto.codigo.includes(filtro)
-    );
-
-    if (produtosFiltrados.length > 0) {
-      let conteudoTabela = '<div class="produto-container">';
-
-      produtosFiltrados.forEach(produto => {
-        conteudoTabela += `
-          <div class="produto-card">
-            <img src="/uploads/${produto.imagem_url}" loading="lazy" alt="${produto.nome}" class="produto-imagem">
-            <div class="produto-info">
-              <h3>${produto.nome}</h3>
-              <p>Código: ${produto.codigo}</p>
-              <p>Quantidade: ${produto.quantidade}</p>
-              ${isAdmin ? `<a href="/barcodes/${produto.codigo}.png" download="${produto.codigo}_barcode.png">Baixar Code</a>` : ''}
-              ${isAdmin ? `<button id="btn_rmv" data-codigo="${produto.codigo}" onclick="removerProduto('${produto.codigo}')">Remover</button>` : ''}
-            </div>
-          </div>`;
-      });
-
-      conteudoTabela += '</div>';
-      tabelaEstoque.innerHTML = conteudoTabela;
+    if (error.message.includes('Código do produto já existe')) {
+      alert('Erro: Produto já existente.');
     } else {
-      tabelaEstoque.innerHTML = '<p>Nenhum produto corresponde ao filtro aplicado.</p>';
+      console.error('Erro ao cadastrar produto:', error);
+      alert(`Erro ao cadastrar produto: ${error.message}`);
     }
-  } catch (error) {
-    console.error('Erro ao atualizar tabela de estoque:', error);
-    alert(`Erro ao atualizar tabela de estoque: ${error.message}`);
   }
 }
 
-// Função para pesquisar produtos
-async function pesquisarProduto(filtro) {
-  try {
-    await atualizarTabelaEstoque(filtro);
-  } catch (error) {
-    console.error('Erro ao pesquisar produtos:', error);
-  }
-}
-
-// Adiciona um event listener para o carregamento inicial do documento
-document.addEventListener('DOMContentLoaded', async () => {
-  await verificarAdmin();
-  atualizarTabelaEstoque();
-
-  // Adiciona um event listener para o campo de pesquisa
-  document.getElementById('pesquisaProduto').addEventListener('input', (event) => {
-    pesquisarProduto(event.target.value);
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('formCadastro').addEventListener('submit', cadastrarProduto);
 });
-
-// Torna as funções acessíveis globalmente
-window.pesquisarProduto = pesquisarProduto;
-window.atualizarTabelaEstoque = atualizarTabelaEstoque;
-window.removerProduto = removerProduto;
